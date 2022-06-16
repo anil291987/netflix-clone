f1902378d006c41ac9131ffabd4909d1fb09a3e3
Joel Kingsley <joelkingsley.r@gmail.com>
2022-06-16 10:48:38 +0530

    Initial Commit

---
+++ Shared/netflix_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflix_cloneApp.swift
+//  Shared
+//
+//  Created by Joel Kingsley on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct netflix_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
